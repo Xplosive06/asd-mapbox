cb20816c3da8bf3d59e43d16cbcdd3460187aa12
Xplosive06 <nottamike92@gmail.com>
2020-01-03 09:22:45 +0100

    Plugin back-end available

--- asd-mapbox.php
+++ asd-mapbox.php
@@ -6,7 +6,54 @@ Version: 0.1
 Author: Mike Notta
 */
 
-add_filter('pre_get_document_title', 'change_the_title');
-function change_the_title($title) {
-    return $title . ' | ASD Mapbox';
+defined( 'ABSPATH' ) or die( 'Hey, what are you doing here? You silly human!' );
+if ( !class_exists( 'AsdMapBox' ) ) {
+	class AsdMapBox
+	{
+		public $plugin;
+		function __construct() {
+			$this->plugin = plugin_basename( __FILE__ );
+		}
+		function register() {
+			add_action( 'admin_enqueue_scripts', array( $this, 'enqueue' ) );
+			add_action( 'admin_menu', array( $this, 'add_admin_pages' ) );
+			add_filter( "plugin_action_links_$this->plugin", array( $this, 'settings_link' ) );
+		}
+		public function settings_link( $links ) {
+			$settings_link = '<a href="admin.php?page=asd-mapbox">Settings</a>';
+			array_push( $links, $settings_link );
+			return $links;
+		}
+		public function add_admin_pages() {
+			add_menu_page( 'ASD MapBOX', 'Asdmapbox', 'manage_options', 'asd-mapbox', array( $this, 'admin_index' ), 'dashicons-store', 110 );
+		}
+		public function admin_index() {
+			require_once plugin_dir_path( __FILE__ ) . 'templates/admin.php';
+		}
+		protected function create_post_type() {
+			add_action( 'init', array( $this, 'custom_post_type' ) );
+		}
+		function custom_post_type() {
+			register_post_type( 'book', ['public' => true, 'label' => 'Books'] );
+		}
+		function enqueue() {
+			// enqueue all our scripts
+			wp_enqueue_style( 'mypluginstyle', plugins_url( '/assets/css/style.css', __FILE__ ) );
+			wp_enqueue_script( 'mypluginscript', plugins_url( '/assets/js/fixstepindicator.js', __FILE__ ) );
+			wp_enqueue_script( 'mypluginscript', plugins_url( '/assets/js/map.js', __FILE__ ) );
+			wp_enqueue_script( 'mypluginscript', plugins_url( '/assets/js/nextprev.js', __FILE__ ) ); 
+			wp_enqueue_script( 'mypluginscript', plugins_url( '/assets/js/showtab.js', __FILE__ ) ); 
+		}
+		function activate() {
+			require_once plugin_dir_path( __FILE__ ) . 'inc/asd-mapbox-activate.php';
+			AsdMapBoxActivate::activate();
+		}
+	}
+	$asdMapBox = new AsdMapBox();
+	$asdMapBox->register();
+	// activation
+	register_activation_hook( __FILE__, array( $asdMapBox, 'activate' ) );
+	// deactivation
+	require_once plugin_dir_path( __FILE__ ) . 'inc/asd-mapbox-deactivate.php';
+	register_deactivation_hook( __FILE__, array( 'AsdMapBoxDeactivate', 'deactivate' ) );
 }
